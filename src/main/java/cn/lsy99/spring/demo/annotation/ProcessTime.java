5201541ace828e3a1151f5bf7e036b3911d4d50e
lisongyuan99 <lisongyuan99@outlook.com>
2020-10-25 00:08:55 +0800

    :recycle: Refactor code

---
+++ src/main/java/cn/lsy99/spring/demo/annotation/ProcessTime.java
@@ -0,0 +1,14 @@
+package cn.lsy99.spring.demo.annotation;
+
+import org.springframework.stereotype.Component;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target(ElementType.METHOD)
+@Retention(RetentionPolicy.SOURCE)
+@Component
+public @interface ProcessTime {
+}
---
+++ src/test/java/cn/lsy99/spring/demo/Controller/DemoControllerTest.java
@@ -0,0 +1,47 @@
+package cn.lsy99.spring.demo.Controller;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.RequestBuilder;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+@SpringBootTest
+public class DemoControllerTest {
+    private MockMvc mvc;
+    @Autowired
+    private WebApplicationContext webApplicationContext;
+    Logger logger = LoggerFactory.getLogger(getClass());
+
+    @BeforeEach
+    public void beforeEach() {
+        logger.info("test start");
+        mvc = MockMvcBuilders.webAppContextSetup(webApplicationContext).build();
+    }
+
+    @Test
+    public void test() throws Exception {
+        logger.info("test");
+        RequestBuilder request = MockMvcRequestBuilders
+                .get("/hello")
+                .param("name", "test")
+                .accept(MediaType.APPLICATION_JSON);
+        MvcResult result = mvc.perform(request)
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andDo(MockMvcResultHandlers.print())
+                .andReturn();
+        logger.info(result.toString());
+    }
+
+}
+
